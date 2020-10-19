61523ddeb0027802f85a6576cd49eb511002fa54
chenjun <chenjundev@gmail.com>
2020-10-19 17:00:59 +0800

    init

---
+++ xdaas-admin/src/main/java/com/xdaas/admin/XdaasAdminApplication.java
@@ -0,0 +1,24 @@
+package com.xdaas.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @Author mathc
+ * @since 2020/10/19
+ */
+@SpringBootApplication
+
+public class XdaasAdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdaasAdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XdaasAdminApplication.class);
+    }
+}

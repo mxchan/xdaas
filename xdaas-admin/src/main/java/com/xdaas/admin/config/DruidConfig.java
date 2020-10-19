4ade2b0e05deca611df9387f699099e84ff6e593
chenjun <chenjundev@gmail.com>
2020-10-19 18:43:22 +0800

    mybatis plus

--- xdaas-admin/src/main/java/com/xdaas/admin/config/DruidConfig.java
+++ xdaas-admin/src/main/java/com/xdaas/admin/config/DruidConfig.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
 public class DruidConfig {
 
     @Bean
-    @ConfigurationProperties("spring.datasource.druid")
+    @ConfigurationProperties("spring.datasource")
     public DataSource getDatasource() {
         return new DruidDataSource();
     }

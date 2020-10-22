5279cc31364cc76e0a465d4f273b1610d4f93bf2
chenjun <chenjundev@gmail.com>
2020-10-22 17:59:59 +0800

    security

--- xdaas-admin/src/test/java/com/xdaas/admin/test/UserTest.java
+++ xdaas-admin/src/test/java/com/xdaas/admin/test/UserTest.java
@@ -4,7 +4,6 @@ import com.xdaas.admin.dal.dataobj.SysUserDO;
 import com.xdaas.admin.dal.mapper.SysUserMapper;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
